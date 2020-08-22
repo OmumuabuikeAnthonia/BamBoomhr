92a6a14dafc85b611e9c9551cee78a07abee96c2
OmumuabuikeAnthonia <toniabuike@gmail.com>
2020-08-22 22:55:17 +0100

    Human Resource web application

---
+++ BamBoomhr/Models/EmployeeContactDetails.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BamBoomhr.Models
+{
+    public class EmployeeContactDetails
+    {
+
+        public int Id { get; set; }
+        public string HomeTelephone { get; set; }
+        public string Mobile { get; set; }
+        public string WorkTelephone { get; set; }
+        public string WorkEmail { get; set; }
+        public string OtherEmail { get; set; }
+        public string Phone { get; set; }
+        public string Fax { get; set; }
+        public string Email { get; set; }
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        public string ZipCode { get; set; }
+        public string Country { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+        public int UserId { get; set; }
+    }
+}
---
+++ BamBoomhr/Models/EmployeeEmergencyContact.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BamBoomhr.Models
+{
+    public class EmployeeEmergencyContact
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Relationship { get; set; }
+        public string HomeTelephone { get; set; }
+        public string Mobile { get; set; }
+        public string WorkTelephone { get; set; }
+        public DateTime DateCreated { get; set; }
+        public DateTime DateModified { get; set; }
+        public int UserId { get; set; }
+    }
+}
